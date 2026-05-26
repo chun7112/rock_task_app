23462e428a3aef5ddcb36581dec63df46c593383
鄭妤君 <kasey@zhengyujundeMacBook-Air.local>
2026-05-26 13:38:45 +0800

    for phone

--- lib/api_config.dart
+++ lib/api_config.dart
@@ -1,4 +1,4 @@
-const bool useLocalhost = true; // 👉 手機用 false
+const bool useLocalhost = false; // 👉 手機用 false
 
 const String localUrl = "http://127.0.0.1:5000";
 const String deviceUrl = "http://172.20.10.4:5000"; // 👉 換成你的IP
